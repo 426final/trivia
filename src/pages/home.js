5f61e81e0b5c01872cf4b088f95da5425b48d0fd
Ella Juengst <ellaj@live.unc.edu>
2020-11-22 23:53:48 -0500

    adding better svg

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
       <div className="home">
         <div className="trivia-home-div">
-            <img className="trivia-home" src={require('../assets/trivia-home.svg')}></img>
+            <img className="trivia-home" src={require('../assets/trivia-home1.svg')}></img>
         </div>
       </div>
       
